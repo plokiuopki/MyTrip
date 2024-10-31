a9870dee81f3009e2b8a78e96a0e98d39b7ddeea
plokiuopki <jessicalashea.1@gmail.com>
2024-10-31 14:57:28 -0400

    Update MyTrip.java

--- MyTrip.java
+++ MyTrip.java
@@ -1,4 +1,3 @@
-// MyTrip.java
 import java.util.Scanner;
 
 public class MyTrip {
@@ -7,25 +6,24 @@ public class MyTrip {
 
         System.out.println("Welcome to My Trip Planner!");
 
-        // Create instances of each component
+        // Creates instances of each class so it can be used to calculate overall trip cost
         BudgetManager budgetManager = new BudgetManager(scanner);
         Transportation transportation = new Transportation(scanner);
         Accommodation accommodation = new Accommodation(scanner);
         Excursion excursion = new Excursion(scanner);
 
-        // Collect user information
+        // Assigns new variables for collected user information
         double budget = budgetManager.askBudget();
         String travelDate = budgetManager.askTravelDate();
         String destination = budgetManager.askDestination();
-        //boolean travelingAlone = budgetManager.askTravelingAlone();
 
-        // Calculate costs
+        // Assigns new variables for calculated travel costs
         double airfare = transportation.calculateAirfare(travelDate);
         double insurance = transportation.askTravelInsurance(airfare);
         double hotelCost = accommodation.calculateHotel();
         double excursionCost = excursion.calculateExcursions(destination, budget - (airfare + insurance + hotelCost));
 
-        // Display final itinerary
-        budgetManager.printItinerary(travelDate, destination, airfare, insurance, hotelCost, excursionCost);
+        // Display final itinerary to user
+        budgetManager.printItinerary(travelDate, budget, destination, airfare, insurance, hotelCost, excursionCost);
     }
 }
